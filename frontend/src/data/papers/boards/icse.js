bfa21a6af841f32591496ffc5c494656c4a34aaa
ujjwal <pointsdate@gmail.com>
2025-08-22 00:37:31 +0530

    ICSE Papers: add real paper links for Class 10 subjects (2017-2020) and update Class 12 subjects

--- frontend/src/data/papers/boards/icse.js
+++ frontend/src/data/papers/boards/icse.js
@@ -10,80 +10,87 @@ const ICSE = {
           id: 'biology',
           name: 'Biology',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1XAglUHaqA37Gnm8VbQfgdwyISGAZp-4V/view?usp=drive_link' },
+            { year: '2018', link: 'https://drive.google.com/file/d/1XBqexxATbU7yXCUoEJTmRp0EtaZqwpHg/view?usp=drive_link' },{ year: '2019', link: 'https://drive.google.com/file/d/1XDff8jR7vzjnGp0Zqk1oINwkhv_i97oh/view?usp=drive_link' },
+            { year: '2020', link: 'https://drive.google.com/file/d/1X9E0URp-60qKzkVmuFRNpFGUQIeButLe/view?usp=drive_link' }
           ],
         },
         {
           id: 'chemistry',
           name: 'Chemistry',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1XMLyMaUjc5rV8Uwl6S065m-mHjTHMr2J/view?usp=drive_link' },
+            { year: '2018', link: 'https://drive.google.com/file/d/1XQeSb5CKYf7pT1TcqRuS3sbx5O4rEXnT/view?usp=drive_link' },{ year: '2019', link: 'https://drive.google.com/file/d/1XeSEn-0WPnmK1BA1JvoCDoXw5ZIusf1U/view?usp=drive_link' },
+            { year: '2020', link: 'https://drive.google.com/file/d/1XIElcLeVwn5BE8ElAlNd1kTzLgRhq-Mr/view?usp=drive_link' }
           ],
         },
         {
           id: 'computer',
-          name: 'Computer',
+          name: 'Computer applications',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1WuRYqeHeowS9jLTpP1IO60DXPsgkCHP7/view?usp=drive_link' },
+            { year: '2018', link: 'https://drive.google.com/file/d/1X71wde3mrNIjD6Q7wocdXw8c0Cw7IpNF/view?usp=drive_link' },{ year: '2019', link: 'https://drive.google.com/file/d/1X7mYu8jufckIbTHPm_Q47vkz6NoOBloE/view?usp=drive_link' },
+            { year: '2020', link: 'https://drive.google.com/file/d/1WuNubgMv_FTrDMBP3IHRtkxlKOvsW8d7/view?usp=drive_link' }
           ],
         },
         {
           id: 'economics',
           name: 'Economics',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1VwO_4pujWdkQhVa9hVSWNxZfJqfNmLKI/view?usp=drive_link' },
+            { year: '2018', link: 'https://drive.google.com/file/d/1W0O5aJ0RW-IoEr2_fNQKAf6cSWPHOXuU/view?usp=drive_link' },{ year: '2019', link: 'https://drive.google.com/file/d/1W7Ln2t76Fghg9VAUVAtCVvhc0szE8GgW/view?usp=drive_link' },
+            { year: '2020', link: 'https://drive.google.com/file/d/1Vp8mjGBjbbCE2kpOIr0z91dNbVPtzLnh/view?usp=drive_link' }
           ],
         },
         {
           id: 'english',
           name: 'English',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017 Language', link: 'https://drive.google.com/file/d/1WIlzPI4iTIJ1kxbyR-YIapCjfeVqOqZ5/view?usp=drive_link' },
+            { year: '2017 Literature', link: 'https://drive.google.com/file/d/1WjbxM53-1l1-9yyBEE0r71zjt92ADEDH/view?usp=drive_link' },{ year: '2018 Language', link: 'https://drive.google.com/file/d/1WOeH3SR9MuzydZhwOaM2lTf3hLYhdDGq/view?usp=drive_link' },{ year: '2018 Literature', link: 'https://drive.google.com/file/d/1WlYNTbfAimaaI9HMSFAskJG2-ma9OFUT/view?usp=drive_link' },{ year: '2019 Language', link: 'https://drive.google.com/file/d/1WUlny1SM9ctgcD7RBEcjOVV8YlYns1LL/view?usp=drive_link' },{ year: '2019 Literature', link: 'https://drive.google.com/file/d/1WquQe5wtiyLGfLIF7_dx_bprrN0wc5xt/view?usp=drive_link' },{ year: '2020 Language', link: 'https://drive.google.com/file/d/1WY372c1aVM1Q9ISHpH8MdwqpKJrLbC4c/view?usp=drive_link' },{ year: '2020 Literature', link: 'https://drive.google.com/file/d/1WGb2_fdz40-f0iwxpalguQBtTnMGfAqu/view?usp=drive_link' }
           ],
         },
         {
           id: 'geography',
           name: 'Geography',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1VgsvS5a8jjB22PURLGxqMwF7Spgiglxh/view?usp=drive_link' },
+            { year: '2019', link: 'https://drive.google.com/file/d/1VndRXmZjmY6qtu_6I9m3DPkFrDhb9Oz7/view?usp=drive_link' },{ year: '2020', link: 'https://drive.google.com/file/d/1Vgg3IS95KABa9ovFN1QS_1t30XQVf6cb/view?usp=drive_link' },
           ],
         },
         {
           id: 'hindi',
           name: 'Hindi',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1WFx1C4Nb8L90LaJDOJm8IYiIru7Mk--q/view?usp=drive_link' },
+            { year: '2019', link: 'https://drive.google.com/file/d/1WA_5jP--mhGKFGetj4GkuXsspyYZWlrX/view?usp=drive_link' },{ year: '2020', link: 'https://drive.google.com/file/d/1WCMLCnBpUOlcez5MNHjlBMTEM6uKu9U1/view?usp=drive_link' },
           ],
         },
         {
           id: 'history_and_civics',
           name: 'History and Civics',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1VeRbtk0R8SldE25CfUcCeFqr3y3Ufipt/view?usp=drive_link' },
+            { year: '2018', link: 'https://drive.google.com/file/d/1V_AEKU5V8vckZzscHo1hgxtPjntAhvr9/view?usp=drive_link' },{ year: '2019', link: 'https://drive.google.com/file/d/1VgIPlyZCFqEI3TDjgaFrl4UUOY36Zbkb/view?usp=drive_link' },
+            { year: '2020', link: 'https://drive.google.com/file/d/1VfuRl2WwJxJQJbM8ZyRZo7F3lhGtXBrl/view?usp=drive_link' }
           ],
         },
         {
           id: 'mathematics',
           name: 'Mathematics',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1YNSdJNe-yRigUxs1HuhBmzMFoQ0LoUWv/view?usp=drive_link' },
+            { year: '2018', link: 'https://drive.google.com/file/d/1YPhdhfM_IzmpVrDun_b3eTcj3INUunkA/view?usp=drive_link' },{ year: '2019', link: 'https://drive.google.com/file/d/1YURyuMLSV6PX0eom1EkKELorYlb6qiUa/view?usp=drive_link' },
+            { year: '2020', link: 'https://drive.google.com/file/d/1YLqn9kY8z5KMKHKhvB3VWwl7DdDtZ3sE/view?usp=drive_link' }
           ],
         },
         {
           id: 'physics',
           name: 'Physics',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017', link: 'https://drive.google.com/file/d/1XxwyctNBA9VtDgaiOoCoioDEhjPT1nU-/view?usp=drive_link' },
+            { year: '2018', link: 'https://drive.google.com/file/d/1YBw_nYbPPcdZ_3cWrSgdWE2Y_C_sld1J/view?usp=drive_link' },{ year: '2019', link: 'https://drive.google.com/file/d/1YDee1UdGni4x8h1Tumwa0LCrB54O2VIW/view?usp=drive_link' },
+            { year: '2020', link: 'https://drive.google.com/file/d/1XhQx9cjsd5umymZrbjrbR6WKGUjlPxEa/view?usp=drive_link' }
           ],
         },
       ],
@@ -97,7 +104,8 @@ const ICSE = {
           name: 'Biology',
           papers: [
             { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2023', link: '#' },{ year: '2024', link: '#' },
+            { year: '2023', link: '#' }
           ],
         },
         {
@@ -105,7 +113,8 @@ const ICSE = {
           name: 'Chemistry',
           papers: [
             { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2023', link: '#' },{ year: '2024', link: '#' },
+            { year: '2023', link: '#' }
           ],
         },
         {
@@ -113,7 +122,8 @@ const ICSE = {
           name: 'Computer Science',
           papers: [
             { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2023', link: '#' },{ year: '2024', link: '#' },
+            { year: '2023', link: '#' }
           ],
         },
         {
@@ -121,7 +131,8 @@ const ICSE = {
           name: 'English',
           papers: [
             { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2023', link: '#' },{ year: '2024', link: '#' },
+            { year: '2023', link: '#' }
           ],
         },
         {
@@ -129,7 +140,8 @@ const ICSE = {
           name: 'Hindi',
           papers: [
             { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2023', link: '#' },{ year: '2024', link: '#' },
+            { year: '2023', link: '#' }
           ],
         },
         {
@@ -137,7 +149,8 @@ const ICSE = {
           name: 'Mathematics',
           papers: [
             { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2023', link: '#' },{ year: '2024', link: '#' },
+            { year: '2023', link: '#' }
           ],
         },
         {
@@ -145,7 +158,8 @@ const ICSE = {
           name: 'Physical Education',
           papers: [
             { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2023', link: '#' },{ year: '2024', link: '#' },
+            { year: '2023', link: '#' }
           ],
         },
         {
@@ -153,7 +167,8 @@ const ICSE = {
           name: 'Physics',
           papers: [
             { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2023', link: '#' },{ year: '2024', link: '#' },
+            { year: '2023', link: '#' }
           ],
         },
       ],
