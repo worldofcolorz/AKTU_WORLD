f4c5d4f09f986edb6447479439f70fef5e150f8e
ujjwal <pointsdate@gmail.com>
2025-08-22 23:30:01 +0530

    cbse 12 subjects links added

--- frontend/src/data/papers/boards/cbse.js
+++ frontend/src/data/papers/boards/cbse.js
@@ -74,88 +74,111 @@ const CBSE = {
           id: 'biology',
           name: 'Biology',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1gXjuNE5kjRRt3Piw_VHIbPkebgfbHZZO/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1gZ5VUSHg1fdXpQOgCxuNGCFsabQAvfoE/view?usp=drive_link' },{ year: '2019 set 1', link: 'https://drive.google.com/file/d/1fuss6WJaBlFiON3v_VLa4VkwrbtAYeDZ/view?usp=drive_link' },
+            { year: '2020 set 1', link: 'https://drive.google.com/file/d/1e4ZHF13iMs9T1enbHPkTyMYbRzspbTwj/view?usp=drive_link' },{ year: '2021 set 1', link: 'https://drive.google.com/file/d/1dUmaXOgm1TMFL7MpCBwwdl0i8iKLabmv/view?usp=drive_link' },
+            { year: '2022 set 1', link: 'https://drive.google.com/file/d/1cFINLCyUo-uxdwN-uQS4b7_2aDOGPjQc/view?usp=drive_link' },{ year: '2023 set 1', link: 'https://drive.google.com/file/d/1b52F-PkYv8HTjaJRiifc-a1LuX3crrtS/view?usp=drive_link' }
           ],
         },
         {
           id: 'chemistry',
           name: 'Chemistry',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1gpWhVpDVMa98ttn9VH79XKXgBbdgyddS/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1h4LKvREwrsPXqfyRE1z_-h_MHH58V3oD/view?usp=drive_link' },{ year: '2018 set 1', link: 'https://drive.google.com/file/d/1gwnXNo9DEuk6yl4oZztajFz8bSJBLzBI/view?usp=drive_link' },
+            { year: '2019 set 1', link: 'https://drive.google.com/file/d/1fnmZhl9TqplvKiO-qlCOI3-ELWDKsqeS/view?usp=drive_link' },{ year: '2020 set 1', link: 'https://drive.google.com/file/d/1eAxciIdNU5yzYXFhCgh6K-HnmYpkVDgC/view?usp=drive_link' },
+            { year: '2021 set 1', link: 'https://drive.google.com/file/d/1dhCwA2Ohv3oDeooIWGNAgCnwvzstSBms/view?usp=drive_link' },{ year: '2022 set 1', link: 'https://drive.google.com/file/d/1cH6xjRIq6V4IngC1lHMXHHFSbds9lfdq/view?usp=drive_link' },
+            { year: '2023 set 1', link: 'https://drive.google.com/file/d/1b5wE7ixLrxts3EQLHttVxm7vPF7e9GqJ/view?usp=drive_link' }
           ],
         },
         {
           id: 'computer_science',
           name: 'Computer Science',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2017 set 4', link: 'https://drive.google.com/file/d/1h7ip1KqAlSJU_AeNjaoHKUB1Ut59BmqA/view?usp=drive_link' },
+            { year: '2019 set 4', link: 'https://drive.google.com/file/d/1fm3q42-zHNoDqiv-8IDN5sVvDhW1ma_Z/view?usp=drive_link' },{ year: '2021 set 4', link: 'https://drive.google.com/file/d/1diyEsdOZJEEK-T7CmP2_B73zFMbn-h74/view?usp=drive_link' },
+            { year: '2022 set 4', link: 'https://drive.google.com/file/d/1ZZPRWvUDcwGXVkDYHC-jS67cfRg5mgEv/view?usp=drive_link' },{ year: '2023 set 4', link: 'https://drive.google.com/file/d/1ZelJMDFvqncLgw88ZNMAL-i28Uu1amke/view?usp=drive_link' },
+            { year: '2024 set 4', link: 'https://drive.google.com/file/d/1ZlWFjSvxPYh8vhed3OCeClmto8fpjbvJ/view?usp=drive_link' },{ year: '2025 set 4', link: 'https://drive.google.com/file/d/1O8a0h-TRKKCk0NWX6kE9iHfU_xw8_2vm/view?usp=drive_link' }
           ],
         },
         {
           id: 'english',
           name: 'English',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1hP97bYfWZ6uR45vIpzq4a-Cs4u1ViNvw/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1hRun5fRbNFo_7ax33jWqFlWs1GMZCth5/view?usp=drive_link' },{ year: '2018 set 1', link: 'https://drive.google.com/file/d/1hRZpbc6KiB36wiT4XDsw25Sfg8RJVriI/view?usp=drive_link' },
+            { year: '2019 set 1', link: 'https://drive.google.com/file/d/1fTsBBpS0B5q_mQjcC-ewrfeA5qya-8QM/view?usp=drive_link' },{ year: '2020 set 1', link: 'https://drive.google.com/file/d/1eDdt2J7hg0hk2a-QtjKcmQ2PNeI2U773/view?usp=drive_link' },
+            { year: '2022 set 1', link: 'https://drive.google.com/file/d/1cN5OagHN6a8jC76Mk0D_dLD6FjAQ__yL/view?usp=drive_link' },{ year: '2023 set 1', link: 'https://drive.google.com/file/d/1bNJ_8osl8Uoq7hEj6AE5yFkRi17YmRQM/view?usp=drive_link' }
           ],
         },
         {
           id: 'hindi',
           name: 'Hindi',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1hS3v7wF-HspkfjWrMZp_jj8onp8FGndr/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1hV10mM79nbsnMvkkxU-vwwTwjAW2Fzpk/view?usp=drive_link' },{ year: '2018 set 1', link: 'https://drive.google.com/file/d/1hSlqxpBTqqhW8YttmLFVNmXzIEE-_brt/view?usp=drive_link' },
+            { year: '2022 set 1', link: 'https://drive.google.com/file/d/1cdKFg_cBL2PP64B7MH9w7XSNoTjxvWRW/view?usp=drive_link' },{ year: '2023 set 1', link: 'https://drive.google.com/file/d/1bglpNnweNwre3p9zjaTzoPa4lBe3Uzb9/view?usp=drive_link' }
           ],
         },
         {
           id: 'mathematics',
           name: 'Mathematics',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1hbEbGDeIkFDx9WVtpwV3TGNAJMRlvOTL/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1hdoh10v0tNPJhpsAy0Mr7QmI_LlUn6o6/view?usp=drive_link' },{ year: '2018 set 1', link: 'https://drive.google.com/file/d/1hcV9lKhCluaH8WsGdRTpSjMM9hEuk5aX/view?usp=drive_link' },
+            { year: '2019 set 1', link: 'https://drive.google.com/file/d/1fKrZztrJUGNddfCfdL_9AfrbEec_x83d/view?usp=drive_link' },{ year: '2021 set 1', link: 'https://drive.google.com/file/d/1donpvZMvYfoMg9yM9B9l5dADcNQS2ojn/view?usp=drive_link' },
+            { year: '2022 set 1', link: 'https://drive.google.com/file/d/1ccpu2DeyREhyyLY2jZlQAV7B0kGuSA9t/view?usp=drive_link' },{ year: '2023 set 1', link: 'https://drive.google.com/file/d/1bz9G0R-bRp6ot05iuOALZHz4DXRAZxE9/view?usp=drive_link' }
           ],
         },
         {
           id: 'physical_education',
           name: 'Physical Education',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2021 set 4', link: 'https://drive.google.com/file/d/1dsrb-fq-jRNM7JPNF2Lx1l-SzU8MhQqw/view?usp=drive_link' },
+            { year: '2022 set 4', link: 'https://drive.google.com/file/d/1ZcW4Wsfy5ac7qV5oCpvIegWLw5H_xI3i/view?usp=drive_link' },{ year: '2023 set 4', link: 'https://drive.google.com/file/d/1ZdzeMr3mBxpkBx6TXDVy1vLrCvCZAaBm/view?usp=drive_link' },
+            { year: '2024 set 4', link: 'https://drive.google.com/file/d/1ZnFGegKGav8w72k9EdxOH-34sJXrzaYY/view?usp=drive_link' },{ year: '2025 set 4', link: 'https://drive.google.com/file/d/1O9K9fkMNTAu_e8fwYComeVGDfS1D6l70/view?usp=drive_link' }
           ],
         },
         {
           id: 'physics',
           name: 'Physics',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1hgnkUwtYxifJRJmXQOYtJ0UABbMRNZIN/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1htYwMYwODYRjnbwsC9POhvy2hFou_f-r/view?usp=drive_link' },{ year: '2018 set 1', link: 'https://drive.google.com/file/d/1hlalwcNCl1WpD4QMMrNdD5RizLRK5xq_/view?usp=drive_link' },
+            { year: '2019 set 1', link: 'https://drive.google.com/file/d/1fFv4Se-oqfqVUBmLgVz5yGdOHRsEitZQ/view?usp=drive_link' },{ year: '2020 set 1', link: 'https://drive.google.com/file/d/1eF8BeF7XZnbKG1h5HTb-PK1ExcWzE73u/view?usp=drive_link' },
+            { year: '2021 set 1', link: 'https://drive.google.com/file/d/1dt-bq_k81CwEu8ZXmWLrUedxIlqyRvpn/view?usp=drive_link' },{ year: '2022 set 1', link: 'https://drive.google.com/file/d/1cee-VpqYaBIJUd5i-q1ziLWgPYw0iWNk/view?usp=drive_link' },
+            { year: '2023 set 1', link: 'https://drive.google.com/file/d/1c85hgODNO81gWYOYX3pi9DSIUfOaXrdQ/view?usp=drive_link' }
           ],
         },
         {
           id: 'accountancy',
           name: 'Accountancy',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1gQwXKdjB1zB4fI2gT9O-Jj4BwdDawHOJ/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1gMQtLeDzjaQEhWb2LzUKXAS8bWBBUbo6/view?usp=drive_link' },{ year: '2018 set 1', link: 'https://drive.google.com/file/d/1gUPUrbOEtf_zhJD2RubIqRAcE2xDu-LG/view?usp=drive_link' },
+            { year: '2019 set 1', link: 'https://drive.google.com/file/d/1fxoRmeQSW8kvkjFmT1LLuP7BnDGzTBFU/view?usp=drive_link' },{ year: '2020 set 1', link: 'https://drive.google.com/file/d/1dzqBrwpoH78R5_0H7cTxFj6ByHe95m52/view?usp=drive_link' },
+            { year: '2021 set 1', link: 'https://drive.google.com/file/d/1dUQNJSP6rtf-QqYtHV_2jtUUQHZRJ6d_/view?usp=drive_link' },{ year: '2022 set 1', link: 'https://drive.google.com/file/d/1cEgmTC9TIHxgDOYbS-mkO5-yNBxPhau1/view?usp=drive_link' },
+            { year: '2023 set 1', link: 'https://drive.google.com/file/d/1av3iN1blRHakkoFdG7zMBwo3Hfatm_c9/view?usp=drive_link' }
           ],
         },
         {
           id: 'business_studies',
           name: 'Business Studies',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1gb-jS3-dlDOUuVGfSndQy_0nxovlcL7s/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1glS1Av-74a-sxxnHc8c2If9OOQgUa_jc/view?usp=drive_link' },{ year: '2018 set 1', link: 'https://drive.google.com/file/d/1gc1GBusinCI6USCCYWXW53aiXs4hCXcv/view?usp=drive_link' },
+            { year: '2019 set 1', link: 'https://drive.google.com/file/d/1fuS_gLUygAFDI21Uf8BBdSTHc9ApjlEZ/view?usp=drive_link' },{ year: '2021 set 1', link: 'https://drive.google.com/file/d/1dgcKmoG4Tum9ejNJfpT_DstAOhvZJpkG/view?usp=drive_link' },
+            { year: '2022 set 1', link: 'https://drive.google.com/file/d/1cHiBD9bdVtQnNmqC1jWgc5_dHPNywJMP/view?usp=drive_link' },{ year: '2023 set 1', link: 'https://drive.google.com/file/d/1b5o0BmpGSeOhP-Jm28PZOiMtxu31hKuB/view?usp=drive_link' }
           ],
         },
         {
           id: 'economics',
           name: 'Economics',
           papers: [
-            { year: '2024', link: '#' },
-            { year: '2023', link: '#' },
+            { year: '2016 set 1', link: 'https://drive.google.com/file/d/1hLMkFeHT9cjDqoFYiq-TSF10Lu0NvfvS/view?usp=drive_link' },
+            { year: '2017 set 1', link: 'https://drive.google.com/file/d/1hD1TTqL2TpQlkrqgAcv_l7dFaPc91uXm/view?usp=drive_link' },{ year: '2018 set 1', link: 'https://drive.google.com/file/d/1hNdEF9Qz6EFlA9bPqovCpC31nT3h7tqM/view?usp=drive_link' },
+            { year: '2019 set 1', link: 'https://drive.google.com/file/d/1fk3w5NNi1Cl79_ichc68db9c8XTMTkRE/view?usp=drive_link' },{ year: '2020 set 1', link: 'https://drive.google.com/file/d/1eByVNPdqfq7Jmlp5EYHVbqYsOnU3RlyH/view?usp=drive_link' },
+            { year: '2022 set 1', link: 'https://drive.google.com/file/d/1cKz4iCMfCkt9sv9JwjWYrWxuec98e8Qb/view?usp=drive_link' },{ year: '2023 set 1', link: 'https://drive.google.com/file/d/1bIPleQuhSNARQVR-6gdx9tFAALnQUouI/view?usp=drive_link' }
           ],
         },
       ],
